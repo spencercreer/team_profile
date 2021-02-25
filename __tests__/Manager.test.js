6911d8705ff22ee1bf897032fcf833bc0d2ce9e8
spencercreer <spencercreer@gmail.com>
2021-02-24 22:48:08 -0700

    made Employee parent class and other classes extend Employee

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,6 +3,6 @@ const Employee = require('../lib/Employee');
 
 test('getRole should return Manager', () => {
     const testValue = "Manager";
-    const x = new Manager("", );
-    expect(getRole()).toBe(testValue);    
+    const x = new Manager();
+    expect(x.getRole()).toBe(testValue);    
 })
--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const teamMembers = [];
-
+// inquirer promt question arrays
+// add team member prompt question
 const addMemberQuestion = [
     {
         type: "list",
         name: "employeeType",
-        message: "What type of employee would you like to add?",
-        choices: ["Engineer", "Intern", "Finish team"],
+        message: "What would you like to do?",
+        choices: ["Add Engineer", "Add Intern", "Finish team"],
     }
 ];
+// manager prompt questions
 const managerQuestions = [
     { 
         type:"input",
@@ -64,6 +66,7 @@ const managerQuestions = [
         },
     },
 ];
+// engineer prompt questions
 const engQuestions = [
     { 
         type:"input",
@@ -114,6 +117,7 @@ const engQuestions = [
         },
     },
 ];
+// intern prompt questions
 const internQuestions = [
     { 
         type:"input",
@@ -165,53 +169,57 @@ const internQuestions = [
     },
 ]
 
+// prompt to add member and call function based on list choice
 function addMember(){
     console.log("Add a team member")
     inquirer.prompt(addMemberQuestion)
     .then(answer=>{
-        if(answer.employeeType === "Engineer"){
+        if(answer.employeeType === "Add Engineer"){
             createEngineer();
-        } else if(answer.employeeType === "Intern"){
+        } else if(answer.employeeType === "Add Intern"){
             createIntern();
         } else {
+            console.log(teamMembers);
             console.log("End Program");
             // code to end the program
         }
     })
 }
 
+// prompt manager questions then pass to Manager class and push to teamMembers array
 function createManager(){
     console.log("Enter team manager information");
     inquirer.prompt(managerQuestions)
     .then(answer=>{
-        const employee = new Employee(answer.managerName, answer.managerId, answer.managerEmail)
-        const manager = new Manager(answer.managerOffice)
+        const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOffice);
         teamMembers.push(manager);
+        console.log(manager.getEmail());
         addMember();
     })
 }
 
+// prompt engineer questions then pass to Engineer class and push to teamMembers array
 function createEngineer(){
     console.log("Enter engineer's information");
     inquirer.prompt(engQuestions)
     .then(answer=>{
-        const employee = new Employee(answer.engName, answer.engId, answer.engEmail)
-        const engineer = new Engineer(answer.engGithub)
+        const engineer = new Engineer(answer.engName, answer.engId, answer.engEmail, answer.engGithub);
         teamMembers.push(engineer);
         addMember();
     })
 }
 
+// prompt intern questions then pass to Intern class and push to teamMembers array
 function createIntern(){
     console.log("Enter intern's information");
     inquirer.prompt(internQuestions)
     .then(answer=>{
-        const intern = new Intern(answer.internName, answer.internId, answer.internEmail)
-        const intern = new Intern(answer.internSchool)
+        const intern = new Intern(answer.internName, answer.internId, answer.internEmail, answer.internSchool);
         teamMembers.push(intern);
         addMember();
     })
 }
 
+// call createManager function
 createManager();
 
